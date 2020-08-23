3ca483093883e4775a151446a167c75b308d3b48
Sangram Chavan <hello@ambivert.tech>
2020-08-23 15:38:56 +0100

    airi

--- public/content/themes/selfintro/template-parts/page-sections.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Page Sections template
- */
-$post = selfintro_post_variables();
-echo '<div data-scroll="wpdance-'.$post->post_name.'" class="sl-padding page-section'.get_post_type().'-';the_ID(); echo '" id="wpdance-'.$post->post_name.'">';
-        the_content();
-echo '</div>';
-?>      
--- public/content/themes/unrovr/admin/customizer.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-	include_once(get_template_directory() . '/admin/customizer-sections.php');
-	include_once(get_template_directory() . '/admin/customizer-settings.php');
-
-?>
